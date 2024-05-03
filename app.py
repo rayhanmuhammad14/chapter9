fd8d5f52faaf53ef2692a13113cf9a5033227b2b
rayhanmuhammad14 <rayhan10915@gmail.com>
2024-05-03 17:55:44 +0800

    first

---
+++ app.py
@@ -0,0 +1,107 @@
+from flask import (
+    Flask,
+    request,
+    render_template,
+    redirect,
+    url_for,
+    jsonify
+)
+from pymongo import MongoClient
+import requests
+from datetime import datetime
+
+app = Flask(__name__)
+
+cxn_str = 'your-connection-string'
+client = MongoClient(cxn_str)
+
+db = client.dbsparta_plus_week2
+
+@app.route('/')
+def main():
+    words_result = db.words.find({}, {'_id': False})
+    words = []
+    for word in words_result:
+        definition = word['definitions'][0]['shortdef']
+        definition = definition if type(definition) is str else definition[0]
+        words.append({
+            'word': word['word'],
+            'definition': definition,
+        })
+    msg = request.args.get('msg')
+    return render_template(
+        'index.html',
+        words=words,
+        msg=msg
+    )
+
+@app.route('/detail/<keyword>')
+def detail(keyword):
+    api_key = "9019893f-2f67-4954-a00b-c77078b3a050"
+    url = f'https://www.dictionaryapi.com/api/v3/references/collegiate/json/{keyword}?key={api_key}'
+    response = requests.get(url)
+    definitions = response.json()
+
+    if not definitions:
+        return redirect(url_for(
+            'main',
+            msg=f'Could not find {keyword}'
+        ))
+
+    if type(definitions[0]) is str:
+        return redirect(url_for(
+            'main',
+            msg=f'Could not find {keyword}, did you mean {", ".join(definitions)}?'
+        ))
+
+    status = request.args.get('status_give', 'new')
+    return render_template(
+        'detail.html',
+        word=keyword,
+        definitions=definitions,
+        status=status
+    )
+
+@app.route('/api/save_word', methods=['POST'])
+def save_word():
+    json_data = request.get_json()
+    word = json_data.get('word_give')
+    definitions = json_data.get('definitions_give')
+
+    doc = {
+        'word': word,
+        'definitions': definitions,
+        'date': datetime.now().strftime('%Y%m%d'),
+    }
+
+    db.words.insert_one(doc)
+
+    return jsonify({
+        'result': 'success',
+        'msg': f'the word, {word}, was saved!!!',
+    })
+
+@app.route('/api/delete_word', methods=['POST'])
+def delete_word():
+    word = request.form.get('word_give')
+    db.words.delete_one({'word': word})
+    return jsonify({
+        'result': 'success',
+        'msg': f'the word, {word}, was deleted',
+    })
+    
+@app.route('/api/get_exs', methods=['GET'])
+def get_exs():
+    return jsonify({'result': 'success'})
+
+@app.route('/api/save_ex', methods=['POST'])
+def save_ex():
+    return jsonify({'result': 'success'})
+
+
+@app.route('/api/delete_ex', methods=['POST'])
+def delete_ex():
+    return jsonify({'result': 'success'})
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
